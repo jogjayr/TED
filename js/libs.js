e7726c04cd2fa8155865c34437cf54764c98d6d4
Jayraj Jog <jogjayr@gmail.com>
2013-07-14 21:19:50 -0700

    date displays better with month name. thumbnails put into well, maybe doesn't look as good

--- js/libs.js
+++ js/libs.js
@@ -154,15 +154,16 @@ function fileSizeToHumanReadable (fileSize) {
 	if(isNaN(fileSize)) throw new Error('Not a number'); 
 	else {
 		if(fileSize < one_kb) return fileSize + 'bytes';
-		else if(fileSize >= one_kb && fileSize < one_mb) return (fileSize / one_kb).toString() + 'kb';
-		else if(fileSize >= one_mb && fileSize < one_gb) return (fileSize / one_mb).toString() + 'mb';
-		else if(fileSize >= one_gb && fileSize < one_tb) return (fileSize / one_gb).toString() + 'mb';
+		else if(fileSize >= one_kb && fileSize < one_mb) return (fileSize / one_kb).toFixed(2).toString() + ' kb';
+		else if(fileSize >= one_mb && fileSize < one_gb) return (fileSize / one_mb).toFixed(2).toString() + ' mb';
+		else if(fileSize >= one_gb && fileSize < one_tb) return (fileSize / one_gb).toFixed(2).toString() + ' mb';
 	}
 }
 function dateToHumanReadable (date) {
  	var date_obj = new Date(date);
+ 	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	if(date_obj.toString !== 'Invalid Date') {
-		return date_obj.toLocaleString();
+		return date_obj.getDate().toString() + " " + months[date_obj.getMonth()] + ", " + date_obj.getFullYear().toString();
 	}
 	return '';
 }
